2ed84d63f2e42fbe4f18b0ec7f60d17341a62b02
InsupKwak <152236616+insupkwak@users.noreply.github.com>
2024-05-13 22:23:09 +0900

    Add files via upload

--- app_baseball.py
+++ app_baseball.py
@@ -1,5 +1,4 @@
 import json
-import requests
 from flask import Flask, request, jsonify, render_template
 from datetime import datetime, timedelta
 
@@ -7,12 +6,8 @@ app = Flask(__name__)
 data_file = 'attempts.json'
 discord_webhook = "https://discord.com/api/webhooks/1176157989506404512/MPNnjvAJdhmsY37AGexHLQDwgUnekpRSRQKTWHC8_3PMQwrq1u0Z_wB_bR_b1BZHqnSx"
 
-def send_message(msg):
-    now = datetime.now()
-    message = {"content": f"[{now.strftime('%H:%M')}] {str(msg)}"}
-    requests.post(discord_webhook, data=message)
 
-send_message("서버 시작")
+
 
 def load_data():
     try:
@@ -22,18 +17,18 @@ def load_data():
                 attempt['digits'] = str(attempt['digits'])
             return data
     except FileNotFoundError:
-        send_message("파일을 찾을 수 없습니다.")
-        return []
+ 
+        pass
     except json.JSONDecodeError:
-        send_message("JSON 파일을 로드하는 중 오류가 발생했습니다.")
-        return []
+
+        pass
 
 def save_data(data):
     try:
         with open(data_file, 'w') as f:
             json.dump(data, f, default=str)
     except Exception as e:
-        send_message(f"데이터를 저장하는 중 오류가 발생했습니다: {str(e)}")
+         pass
 
 attempts_log = load_data()
 
@@ -58,7 +53,7 @@ def record_attempt():
         return jsonify({'message': 'Data saved successfully', 'data': data}), 201
     except Exception as e:
         print("An error occurred:", e)
-        send_message(f"데이터 기록 중 오류가 발생했습니다: {str(e)}")
+       
         return jsonify({'error': 'An error occurred while processing your request'}), 500
 
 @app.route('/attempts/<int:digits>', methods=['GET'])
@@ -71,8 +66,11 @@ def get_attempts(digits):
         return jsonify(sorted_attempts)
     except Exception as e:
         print("An error occurred:", e)
-        send_message(f"데이터 조회 중 오류가 발생했습니다: {str(e)}")
+
         return jsonify({'error': 'An error occurred while processing your request'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    try:
+        app.run(host='0.0.0.0', debug=False)
+    except Exception as e:
+        print("An error occurred while running the server:", e)
